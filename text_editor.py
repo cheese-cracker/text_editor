29f8982e7dfbcbf9a0f3ea1f4fc65a675ec8b63a
cheese-cracker <4chinmai@gmail.com>
2017-12-18 22:25:17 +0530

    Init: created text editor 2.0

--- text_editor.py
+++ text_editor.py
@@ -4,76 +4,130 @@ import Tkinter as tk
 canvas = Tk()
 
 class text_editize:
- def __init__(self, main, font_pack = ['Courier']):
-  self.main = main
-  self.fonts = font_pack
-  self.title = main.title("Basic Text Editor")
-  self.text_init()
-  self.quitbtn = Button(main, text = 'Quit!', command = self.quit)
-  self.quitbtn.grid(row=3,column=1)
-  self.openbtn = Button(main, text = 'Insert File', command = self.open)
-  self.openbtn.grid(row=2)
-  self.savebtn = Button(main, text = 'Save', command = self.saving)
-  self.savebtn.grid(row=3)
-  self.delbtn = Button(main, text = 'Delete', command = self.delete_lines)
-  self.delbtn.grid(row=2,column=1)
-  self.fontbtn = Button(main, text = 'Font Swap',command = self.font_switch)
-  self.fontbtn.grid(row=1,column=1)
-  self.e = Entry()
-  self.e.grid(row=1)
+    def __init__(self, main, font_pack = ['Courier']):
+        self.main = main
+        self.fonts = font_pack
+        self.title = main.title("Basic Text Editor")
+        self.text_init()
   #self.main.mainloop()?
- def quit(self):
-  self.main.destroy()
- def text_init(self):
-  self.text = Text(self.main)
-  self.f_val = 0
-  self.font_switch()
-  self.text.grid()
- def open(self):
-  global text
-  openlocation = tkFileDialog.askopenfilename()
-  try:
-   f=open(openlocation,"rw")
-  except:
-   print("Can't open!")
-   return
-  self.data = f.readlines()
-  f.close()
-  self.compiled = ''
-  for m in self.data:
-    self.compiled += m
-  try:
-   start = float(self.e.get())
-   self.text.insert(start, self.compiled)
-  except:
-   self.text.insert(END, '\n')
-   self.text.insert(END, self.compiled)
- def saving(self):
-  self.content = self.text.get(1.0, END)
-  savelocation = tkFileDialog.asksaveasfilename()
-  try:
-   f = open(savelocation, "w+")
-   f.write(self.content)
-   f.close()
-  except:
-   print("Couldn't save!")
- def delete_lines(self):
-  try:
-   entry = self.e.get()
-   if entry.lower() == "reset" or entry.lower() == "all":
-    self.text.delete(1.0, END)
-   elif '-' in entry:
-    entry = entry.split("-")
-    self.text.delete(float(entry[0]),float(entry[1])+1)
-   else:
-    self.text.delete(float(entry),float(entry)+1)
-  except:
-   print("Specify Line or type 'reset' for all.")
- def font_switch(self):
-   l = len(self.fonts)
-   self.text.config(font=self.fonts[self.f_val%l])
-   self.f_val += 1
+    def quit(self):
+        self.main.destroy()
+    def text_init(self):
+        self.text = Text(self.main)
+        self.f_val = -1
+        self.font_switch()
+        self.text.grid()
+    def open(self, start):
+        global text
+        openlocation = tkFileDialog.askopenfilename()
+        try:
+            f=open(openlocation,"rw")
+        except:
+            print("Can't open!")
+            return
+        self.data = f.readlines()
+        f.close()
+        self.compiled = ''
+        for m in self.data:
+            self.compiled += m
+        if start:
+            self.text.insert(start, self.compiled)
+        else:
+	    self.text.insert(1.0, self.compiled)
+            #self.text.insert(END, '\n')
+            #self.text.insert(END, self.compiled)
+    def saving(self):
+        self.content = self.text.get(1.0, END)
+        savelocation = tkFileDialog.asksaveasfilename()
+        try:
+            f = open(savelocation, "w+")
+            f.write(self.content)
+            f.close()
+        except:
+            print("Couldn't save!")
+    def font_switch(self,incr=1):
+        l = len(self.fonts)
+        self.f_val += incr
+        self.text.config(font=self.fonts[self.f_val%l])
 
-fonts_list = ['Courier', 'Halvetica', 'Times', 'Verdana']
-text_editize(canvas, fonts_list)
+
+#Dependant class
+class dashboard:
+    def __init__(self, main, homebox):
+        self.main = main
+        self.homebox = homebox
+        self.text = self.homebox.text
+        self.numberize()
+        self.fun = {
+    "delete" : self.delete_lines,
+    "save" : self.homebox.saving,
+    "open" : self.open,
+    "quit" : self.quit,
+    "font+" : self.font_switch,
+    "font-" : self.font_switch,
+    "reset" : self.delete_lines,
+    "!numberize" : self.numberize,
+    "!commands" : self.commands
+}
+        self.e = Entry()
+        self.e.grid(row=1)
+        self.commandbtn = Button(main, text="Enter", command = self.gofigure)
+        self.commandbtn.grid(row=2)
+    def gofigure(self):
+        entry = self.e.get()
+        self.ie = entry.split()
+        try:
+            self.fun[self.ie[0].lower()]()
+            print(self.ie[0].upper())
+        except:
+            print("No Command / Error!")
+    def quit(self):
+        self.main.destroy()
+    def open(self):
+        try:
+            start = float(self.ie[1])
+        except:
+            start = 0
+        self.homebox.open(start)
+    def delete_lines(self):
+        try:
+            entry = self.ie[1].lower()
+            if entry == "all":
+                self.text.delete(1.0, END)
+            elif '-' in entry:
+                entry = entry.split("-")
+                self.text.delete(float(entry[0]),float(entry[1])+1)
+            elif ',' in entry:
+                entry = entry.split(",")
+                for i in range(len(entry)):
+                    self.text.delete(float(entry[i])-i, float(entry[i])-i+1)
+            else:
+                self.text.delete(float(entry),float(entry)+1)
+        except IndexError:
+            if self.ie[0].lower() == "reset":
+                self.text.delete(1.0, END)
+                self.numberize()
+        else:
+            print("Specify Line or type 'all' for all.")
+    def numberize(self):
+        for i in range(1,201):
+            self.text.insert(float(i),str(i)+'\t\n')
+    def font_switch(self):
+        incr = 1
+        if self.ie[0][-1] == '-': incr = -1
+        self.homebox.font_switch(incr)
+    def commands(self):
+        for com in self.fun.keys():
+            print(com)
+        
+# Fonts in (str name, int size=12, str style='normal')
+fonts_list = [('Courier'),
+              ('Halvetica'),
+              ('Times'),
+              ('Symbol'),
+              ('Courier',16),
+              ('Halvetica',16),
+             ]
+mainText = text_editize(canvas, fonts_list)
+Dash = dashboard(canvas, mainText)
 
